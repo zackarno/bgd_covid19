00db8b237f9c240c3fae3594c4e480f164f2a63c
zackarno <zackarno@gmail.com>
2020-04-09 12:38:38 +1200

    put commented scrap code at bottom

--- scripts/hq_validation/prepare_msna_wash_inputs_HQ.R
+++ scripts/hq_validation/prepare_msna_wash_inputs_HQ.R
@@ -16,44 +16,6 @@ kobo_coords<-c("X_gps_reading_longitude",
                "X_gps_reading_latitude")
 
 
-# prepare data sets for hq ------------------------------------------------
-
-
-# msna_ref<-butteR::read_all_csvs_in_folder("../../04_MSNA/2019_BGD_MSNAs/Inputs/Refugee/04_data_analysis/cleaned_data")
-# msna_hh<- msna_ref$`20190915_HH_Refugee_Cleaned_20190917.csv` %>% filter(informed_consent=="yes")
-# msna_indiv<- msna_ref$`20190915_Indiv_Refugee_Cleaned_20190917.csv` %>% filter(X_submission__uuid %in% msna_hh$X_uuid)
-# msna_hh<-msna_hh %>% left_join(msna_ref$`20190915_HH_Refugee_Cleaned_20190917_with_environmental.csv` %>%
-#                                                     select(X_uuid,kobo_coords))
-# msna_indiv_with_gps<- msna_indiv %>% left_join(msna_hh %>% select(X_uuid,kobo_coords),by = c("X_submission__uuid"="X_uuid" ))
-#
-# # dir.create("inputs/inputs_hq")
-# st_as_sf(msna_hh, coords=kobo_coords,crs=4326) %>%
-#   st_join(strata_poly) %>% st_drop_geometry() %>%
-#   write.csv("inputs/inputs_hq/reach_bgd_dataset_2019refugee_msna_HH.csv")
-# st_as_sf(msna_indiv_with_gps, coords=kobo_coords,crs=4326) %>%
-#   st_join(strata_poly) %>% st_drop_geometry() %>%
-#   write.csv("inputs/inputs_hq/reach_bgd_dataset_2019refugee_msna_INDIV.csv")
-#
-#
-# #read all wash hh r 3 clean data
-# wash_r3_clean<-butteR::read_all_csvs_in_folder("../../01_WASH/02_70_DNW_UNICEF_2019/03_WASH_HH_R3/89_rscript/WASH_HH_R3_2019/inputs/WASH_HH_R3/clean_data")
-# #get the raw hh data with coordinates
-# wash_r3_hh_raw<-read.csv("../../01_WASH/02_70_DNW_UNICEF_2019/03_WASH_HH_R3/02_data_collection/clean_data/Header_07july2019.csv",
-#                          stringsAsFactors = FALSE,
-#                          row.names = NULL, na.strings = c(""," ",NA, "NA"),
-#                          strip.white = TRUE)
-#
-# wash_r3_hh_clean<- wash_r3_clean$Header_07july2019.csv %>% left_join(wash_r3_hh_raw %>%
-#                                                                        select(X_uuid,kobo_coords))
-#
-# st_as_sf(wash_r3_hh_clean, coords=kobo_coords,crs=4326) %>%
-#   st_join(strata_poly) %>% st_drop_geometry() %>%
-#   write.csv("inputs/inputs_hq/reach_bgd_dataset_2019refugee_wash_hh_r3_HH.csv")
-
-# wash_r3_clean$Container_19June2019.csv %>% write.csv("inputs/inputs_hq/reach_bgd_dataset_2019refugee_wash_hh_r3_CONTAINER.csv")
-# wash_r3_clean$Individual_19June2019.csv %>% write.csv("inputs/inputs_hq/reach_bgd_dataset_2019refugee_wash_hh_r3_INDIV.csv")
-
-
 # read in data sets -------------------------------------------------------
 
 
@@ -137,4 +99,48 @@ wash_hh<- wash_hh %>%
 print("WASH HH object is called wash_hh")
 print("MSNA HH  object is called msna_hh")
 print("MSNA INDIV  object is called msna_indiv")
+
+
+
+
+
+
+# prepare data sets for hq -scrap now ------------------------------------------------
+
+
+# msna_ref<-butteR::read_all_csvs_in_folder("../../04_MSNA/2019_BGD_MSNAs/Inputs/Refugee/04_data_analysis/cleaned_data")
+# msna_hh<- msna_ref$`20190915_HH_Refugee_Cleaned_20190917.csv` %>% filter(informed_consent=="yes")
+# msna_indiv<- msna_ref$`20190915_Indiv_Refugee_Cleaned_20190917.csv` %>% filter(X_submission__uuid %in% msna_hh$X_uuid)
+# msna_hh<-msna_hh %>% left_join(msna_ref$`20190915_HH_Refugee_Cleaned_20190917_with_environmental.csv` %>%
+#                                                     select(X_uuid,kobo_coords))
+# msna_indiv_with_gps<- msna_indiv %>% left_join(msna_hh %>% select(X_uuid,kobo_coords),by = c("X_submission__uuid"="X_uuid" ))
+#
+# # dir.create("inputs/inputs_hq")
+# st_as_sf(msna_hh, coords=kobo_coords,crs=4326) %>%
+#   st_join(strata_poly) %>% st_drop_geometry() %>%
+#   write.csv("inputs/inputs_hq/reach_bgd_dataset_2019refugee_msna_HH.csv")
+# st_as_sf(msna_indiv_with_gps, coords=kobo_coords,crs=4326) %>%
+#   st_join(strata_poly) %>% st_drop_geometry() %>%
+#   write.csv("inputs/inputs_hq/reach_bgd_dataset_2019refugee_msna_INDIV.csv")
+#
+#
+# #read all wash hh r 3 clean data
+# wash_r3_clean<-butteR::read_all_csvs_in_folder("../../01_WASH/02_70_DNW_UNICEF_2019/03_WASH_HH_R3/89_rscript/WASH_HH_R3_2019/inputs/WASH_HH_R3/clean_data")
+# #get the raw hh data with coordinates
+# wash_r3_hh_raw<-read.csv("../../01_WASH/02_70_DNW_UNICEF_2019/03_WASH_HH_R3/02_data_collection/clean_data/Header_07july2019.csv",
+#                          stringsAsFactors = FALSE,
+#                          row.names = NULL, na.strings = c(""," ",NA, "NA"),
+#                          strip.white = TRUE)
+#
+# wash_r3_hh_clean<- wash_r3_clean$Header_07july2019.csv %>% left_join(wash_r3_hh_raw %>%
+#                                                                        select(X_uuid,kobo_coords))
+#
+# st_as_sf(wash_r3_hh_clean, coords=kobo_coords,crs=4326) %>%
+#   st_join(strata_poly) %>% st_drop_geometry() %>%
+#   write.csv("inputs/inputs_hq/reach_bgd_dataset_2019refugee_wash_hh_r3_HH.csv")
+
+# wash_r3_clean$Container_19June2019.csv %>% write.csv("inputs/inputs_hq/reach_bgd_dataset_2019refugee_wash_hh_r3_CONTAINER.csv")
+# wash_r3_clean$Individual_19June2019.csv %>% write.csv("inputs/inputs_hq/reach_bgd_dataset_2019refugee_wash_hh_r3_INDIV.csv")
+
+
 
