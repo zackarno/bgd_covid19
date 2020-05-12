fbf7df43885505eecd86c355fa70915d7f3f7ace
zackarno <zackarno@gmail.com>
2020-05-12 19:07:11 +1200

    finished round comparison and rank data merge questions

--- BGD_2020_Markets_Covid/scripts/data_merge_multi_round_comparisons.R
+++ BGD_2020_Markets_Covid/scripts/data_merge_multi_round_comparisons.R
@@ -44,12 +44,10 @@ round_number<-length(clean_data_file_paths)
 current_round <- read.csv(clean_data_file_paths[round_number], stringsAsFactors = FALSE,
                        na.strings = c("", " ", NA))
 
+current_round$rice_unit
+current_round$price_of_1kg
 prev_round <- read.csv(clean_data_file_paths[round_number-1], stringsAsFactors = FALSE,
                        na.strings = c("", " ", NA))
-
-
-
-
 
 
 # automatically load tool------------------------
@@ -64,6 +62,14 @@ kc<-readxl::read_xlsx(path = xlsform_paths[round_number],sheet = "choices")
 # will make this better and add to butteR
 
 current_round<-refactor_to_xlsform(data = current_round,kobo_survey = ks,kobo_choices = kc,label_column = "label::english" )
+xls_lt<-make_xlsform_lookup_table(kobo_survey = ks,kobo_choices = kc,label_column = "label::english")
+
+# current_round<- current_round %>% rename(items_are_most_affected.dry_fish="items_are_most_affected.fish",
+#                                          sell_oil="cooking_oil",
+#                                          items_are_most_affected.washing_powder="items_are_most_affected.powder",
+#                                          sell_rice="selling_rice")
+
+
 
 
 
@@ -95,7 +101,46 @@ prev_analysis<-butteR::mean_proportion_table(design = dfsvy_prev,list_of_variabl
 page_3_fs<-percentage_pt_change(x = current_analysis, y=prev_analysis)
 page_3_fs_dm<-cbind(page_3_fs$percent,page_3_fs$triangle_cols)
 
+dm_output<-list()
+dm_output[["p3_tables"]]<-page_3_fs_dm
 
+
+selling_assistance_items<-current_round %>% select(starts_with("assistance_items_if_yes.")) %>% colnames()
+
+top_n_assistance_items<-return_top_sm(df = current_round,rank_col = "assistance_items_if_yes",rank_n = 3,xlsform_lookup = xls_lt)
+
+dm_output[["top_assistance_items"]]<-top_n_assistance_items$wide
+
+selling_affected_table<-tibble(
+
+  selling_item=c("sell_bananas", "sell_chicken", "sell_eggs", "sell_dry_fish",
+                 "sell_leafy_greens", "sell_lentils","cooking_oil", "sell_paracetamol", "sell_washing_powder",
+                 "selling_rice",  "sell_soap",
+                 "sell_tarpaulin" ),
+
+  affected_item= c("items_are_most_affected.bananas", "items_are_most_affected.Chicken",
+                   "items_are_most_affected.eggs", "items_are_most_affected.fish",
+                   "items_are_most_affected.leafy_greens", "items_are_most_affected.lentils",
+                   "items_are_most_affected.oil", "items_are_most_affected.paracetamol",
+                   "items_are_most_affected.powder", "items_are_most_affected.rice",
+                   "items_are_most_affected.soap", "items_are_most_affected.tarpaulin")
+)
+
+
+
+items_affected_subset_list<-list()
+for(i in 1: nrow(selling_affected_table)){
+  sell_item_yn<-selling_affected_table[i,1] %>% as.character()
+  affected_item<-selling_affected_table[i,2] %>% as.character()
+  items_affected_subset_list[[i]]<-butteR::mean_proportion_table(design = dfsvy,list_of_variables =c(affected_item) ,aggregation_level = sell_item_yn) %>% filter(!!sym(sell_item_yn)=="yes") %>% select(-1)
+
+}
+
+items_affected_binded<-do.call("cbind",items_affected_subset_list)
+rank_table<-prepare_rank_table(items_affected_binded)
+items_most_affected_ranked<-datamerge_from_rank_table(rank_table = rank_table,xlsform_lookup = xls_lt,rank_n = 6)
+items_most_affected_ranked_labeled<-items_most_affected_ranked$wide %>% select(contains("_lab_"))
+dm_output[["items_most_afftected_rankked"]]<-items_most_affected_ranked_labeled
 
 
 
@@ -107,7 +152,8 @@ food_prices<-c("cheapest_price_for_12_of_chicken",
                 "cheapest_price_for_0.5kg_of_leafy_greens",
                 "cheapest_price_for_1kg_of_bananas",
                 "cheapest_price_for_12__of_eggs",
-                "cheapest_price_for_1kg__of_fish")
+                "cheapest_price_for_1kg__of_fish",
+               "price_of_1kg")
 nfi_prices<-c("cheapest_price_for_100g_soap_bar_of_soap",
                 "cheapest_price_for_0_5l_of_bleachwashing_powder",
                 "cheapest_price_for_12_of_paracetamol",
@@ -136,6 +182,7 @@ prev_round_names_fix<-prev_round %>% rename_at(.vars = item_price_cols_fixed$ite
 current_round<-current_round %>% add_rowwise_nfi_food_medians()
 prev_round_names_fix<-prev_round_names_fix %>% add_rowwise_nfi_food_medians()
 
+
 #make list of vars to calc median
 item_prices_full<-c(item_price_cols_fixed$round_2_items,"i.food_item_median" ,"i.nfi_item_median")
 
@@ -149,11 +196,13 @@ previous_round_median_prices<-prev_round_names_fix %>%
 
 price_differences<-percent_diff_dm(x = current_round_median_prices,y=previous_round_median_prices)
 price_differences_dm<-cbind(price_differences$current_round,price_differences$percent_diff, price_differences$triangle)
+colnames(price_differences_dm)
+price_differences_dm %>% class()
+dm_output[["p1_table_price_differences"]]<-price_differences_dm
 
-#combine wo datamerge outputs
-dm1<-cbind(price_differences_dm,page_3_fs_dm)
 
-# dm1 %>% write.csv("BGD_2020_Markets_Covid/outputs/datamerge/datamerge_20200511.csv", na="",row.names = F)
+datamerge_output_file<-do.call("cbind",dm_output)
+
 
 
 
--- BGD_2020_Markets_Covid/scripts/functions/utils.R
+++ BGD_2020_Markets_Covid/scripts/functions/utils.R
@@ -97,3 +97,154 @@ percent_diff_dm<-function(x,y){
   return(results)
 }
 
+
+
+
+check_dot<-function(x){
+  stringr::str_ends(string = x,pattern ="\\." )
+}
+
+
+prepare_rank_table<-function(rank_cols_analyzed){
+  rank_table<-rank_cols_analyzed %>%
+    pivot_longer(cols = 1:ncol(.),names_to="option", values_to = "value") %>%
+    arrange(desc(value)) %>%
+    mutate(rank_last=rank(desc(value),ties.method = "last")) %>% arrange(rank_last) %>%
+    group_by(value) %>%
+    mutate(num_tie=n()) %>%
+    arrange(rank_last)
+  return(rank_table)
+}
+
+
+
+return_top_sm<-function(df,
+                        rank_col,
+                        rank_n,
+                        tie_break_remove,
+                        aggregation_level=NULL, xlsform_lookup =NULL){
+
+  result<-list()
+  if(check_dot(rank_col)==F){
+    rank_col<-paste0(rank_col,".")
+  }
+  cols_to_rank<-df %>%
+    select(starts_with(rank_col)) %>% colnames()
+  rank_cols_analyzed<- butteR::mean_proportion_table(design = df,list_of_variables = cols_to_rank, aggregation_level=aggregation_level)
+
+  rank_table<- prepare_rank_table(rank_cols_analyzed = rank_cols_analyzed)
+
+  # rank_table<-rank_cols_analyzed %>%
+  #   pivot_longer(cols = 1:ncol(.),names_to="option", values_to = "value") %>%
+  #   arrange(desc(value)) %>%
+  #   mutate(rank_last=rank(desc(value),ties.method = "last")
+  #   ) %>%
+  #   group_by(value) %>%
+  #   mutate(num_tie=n()) %>%
+  #   arrange(rank_last)
+  if(!is.null(xlsform_lookup)){
+    rank_table<-rank_table %>% left_join(xlsform_lookup %>% select(xml_format, question_name,starts_with("choice_label")), by=c("option" ="xml_format"))}
+
+
+  if(rank_table%>% filter(rank_last==rank_n) %>% pull(num_tie)>1){
+    nth_rank_value<-rank_table %>% filter(rank_last==rank_n) %>% pull(value)
+    number_records_with_tie<-rank_table %>% filter(value>=nth_rank_value) %>% nrow()
+    number_records_gt_rank<-number_records_with_tie - rank_n
+    tie_records<- rank_table %>%
+      filter(value==nth_rank_value) %>%
+      mutate(number_to_remove=number_records_gt_rank)
+
+
+    result[["ranks_with_ties"]]<- rank_table
+    result[["ranks_filtered"]]<-rank_table%>% filter(rank_last<=rank_n)
+    result[["tied_records"]]<- tie_records
+    # print(paste("top",rank_n, "in", sm,"were tied", sep = " "))
+    interactive_title<-paste0("You need to remove",number_records_gt_rank, "record(s)", "from the tie_records table")
+    tie_break_remove <- select.list(tie_records$option,multiple = T, title = interactive_title)
+
+    if(!is.null(tie_break_remove)){
+      tie<-result$ranks_with_ties %>%
+        group_by(value) %>%
+        filter(!option%in% tie_break_remove) %>% ungroup() %>%
+        mutate(rank_final= 1:n()) %>% filter(rank_final<=rank_n)
+      result[["ranks_long_for_dm"]]<-tie
+    }else{
+      result[["ranks_long_for_dm"]]<-rank_table%>%
+        filter(rank_last<=rank_n) %>%
+        rename(rank_final="rank_last")
+
+    }
+
+
+    results_long<-result$ranks_long_for_dm %>%
+      mutate(question_rank_lab=paste0(question_name,"_lab_",rank_final),
+             question_rank_val=paste0(question_name,"_val_",rank_final)) %>% ungroup() %>%
+      select(question_rank_lab,question_rank_val, value,starts_with("choice"))
+
+    labels_wide<- results_long %>% pivot_wider(-c(question_rank_val,value), names_from=c("question_rank_lab"), values_from = c("choice_label::english"))
+    values_wide<- results_long %>% pivot_wider(-c(question_rank_lab,"choice_label::english"), names_from=c("question_rank_val"), values_from = c("value"))
+    results_wide<- cbind(labels_wide, values_wide)
+    result[["wide"]]<-results_wide
+
+
+    result$ranks_long_for_dm %>%  pivot_wider(names_from = option, values_from = value)
+
+  }
+  return(result)
+}
+
+
+
+datamerge_from_rank_table<-function(rank_table, xlsform_lookup, rank_n){
+  if(!is.null(xlsform_lookup)){
+    rank_table<-rank_table %>% left_join(xlsform_lookup %>% select(xml_format, question_name,starts_with("choice_label")), by=c("option" ="xml_format"))}
+
+
+  if(rank_table%>% filter(rank_last==rank_n) %>% pull(num_tie)>1){
+    result<-list()
+    nth_rank_value<-rank_table %>% filter(rank_last==rank_n) %>% pull(value)
+    number_records_with_tie<-rank_table %>% filter(value>=nth_rank_value) %>% nrow()
+    number_records_gt_rank<-number_records_with_tie - rank_n
+    tie_records<- rank_table %>%
+      filter(value==nth_rank_value) %>%
+      mutate(number_to_remove=number_records_gt_rank)
+
+
+    result[["ranks_with_ties"]]<- rank_table
+    result[["ranks_filtered"]]<-rank_table%>% filter(rank_last<=rank_n)
+    result[["tied_records"]]<- tie_records
+    # print(paste("top",rank_n, "in", sm,"were tied", sep = " "))
+    interactive_title<-paste0("You need to remove",number_records_gt_rank, "record(s)", "from the tie_records table")
+    tie_break_remove <- select.list(tie_records$option,multiple = T, title = interactive_title)
+
+    if(!is.null(tie_break_remove)){
+      tie<-result$ranks_with_ties %>%
+        group_by(value) %>%
+        filter(!option%in% tie_break_remove) %>% ungroup() %>%
+        mutate(rank_final= 1:n()) %>% filter(rank_final<=rank_n)
+      result[["ranks_long_for_dm"]]<-tie
+    }else{
+      result[["ranks_long_for_dm"]]<-rank_table%>%
+        filter(rank_last<=rank_n) %>%
+        rename(rank_final="rank_last")
+
+    }
+
+
+    results_long<-result$ranks_long_for_dm %>%
+      mutate(question_rank_lab=paste0(question_name,"_lab_",rank_final),
+             question_rank_val=paste0(question_name,"_val_",rank_final)) %>% ungroup() %>%
+      select(question_rank_lab,question_rank_val, value,starts_with("choice"))
+
+    labels_wide<- results_long %>% pivot_wider(-c(question_rank_val,value), names_from=c("question_rank_lab"), values_from = c("choice_label::english"))
+    values_wide<- results_long %>% pivot_wider(-c(question_rank_lab,"choice_label::english"), names_from=c("question_rank_val"), values_from = c("value"))
+    results_wide<- cbind(labels_wide, values_wide)
+    result[["wide"]]<-results_wide
+
+
+    result$ranks_long_for_dm %>%  pivot_wider(names_from = option, values_from = value)
+
+  }
+  return(result)
+}
+
